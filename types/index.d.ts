bd5a2706a19aa8d9528b17ca06d84d658b47c62a
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-01-25 10:19:20 +0100

    feat(typescript): improve types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,8 @@
 declare module 'react-easy-state' {
-  function store(obj?: object): object
-  function view(comp: Function): Function
+  import { ComponentType } from 'react'
+
+  // takes an object (optionally), wraps it in a transparent proxy and returns the proxy
+  function store<T extends object>(obj?: T): T
+  // takes class or function component and returns a class HOC
+  function view<C extends ComponentType<any>>(comp: C): C
 }
