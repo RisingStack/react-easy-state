14d858ab42a6760913cf8957acfd44aa115a5c83
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-05-16 20:05:28 +0200

    feat: add deriveStoresFromProps lifecyle method

--- __tests__/edgeCases.test.js
+++ __tests__/edgeCases.test.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { view, store } from 'react-easy-state'
 import { mount } from 'enzyme'
 
-describe('errors', () => {
+describe('edge cases', () => {
   test('view() should respect shouldComponentUpdate', () => {
     const person = store({ name: 'Bob' })
     const MyComp = view(
@@ -20,30 +20,6 @@ describe('errors', () => {
     expect(comp.text()).toBe('Bob')
     person.name = 'Ann'
     expect(comp.text()).toBe('Bob')
-  })
-
-  test('store() should throw on non object or undefined argument', () => {
-    expect(() => store(null)).toThrow()
-    expect(() => store({})).not.toThrow()
-    expect(() => store()).not.toThrow()
-  })
-
-  test('should not change vanilla setState behavior', () => {
-    const MyComp = view(
-      class extends Component {
-        state = { counter: 0 };
-        increment = () => this.setState({ counter: this.state.counter + 1 });
-
-        render () {
-          return <div onClick={this.increment}>{this.state.counter}</div>
-        }
-      }
-    )
-
-    const comp = mount(<MyComp />)
-    expect(comp.text()).toBe('0')
-    comp.find('div').simulate('click')
-    expect(comp.text()).toBe('1')
   })
 
   test('should not change vanilla setState behavior', () => {
@@ -88,5 +64,57 @@ describe('errors', () => {
     expect(RawChild.mock.calls.length).toBe(1)
     comp.find('button').simulate('click')
     expect(RawChild.mock.calls.length).toBe(1)
+  })
+
+  test('view() should respect custom deriveStoresFromProps', () => {
+    const MyComp = view(
+      class extends Component {
+        store1 = store({ num: 0 });
+        store2 = store({ num: 1 });
+
+        static deriveStoresFromProps (props, store1, store2) {
+          store1.num = props.num1 || store1.num
+          store2.num = props.num2 || store2.num
+        }
+        onClick = () => this.store1.num++;
+
+        render () {
+          return (
+            <div onClick={this.onClick}>
+              {this.store1.num}
+              {this.store2.num}
+            </div>
+          )
+        }
+      }
+    )
+
+    const comp = mount(<MyComp num1={1} />)
+    expect(comp.text()).toBe('11')
+    comp.find('div').simulate('click')
+    expect(comp.text()).toBe('21')
+    comp.setProps({ num2: 4, num1: undefined })
+    expect(comp.text()).toBe('24')
+    comp.find('div').simulate('click')
+    expect(comp.text()).toBe('34')
+  })
+
+  test('view() should respect getDerivedStateFromProps', () => {
+    const MyComp = view(
+      class extends Component {
+        state = { num: 2 };
+        static getDerivedStateFromProps (props, state) {
+          return {
+            num: props.num || state.num
+          }
+        }
+        render () {
+          return <div>{this.state.num}</div>
+        }
+      }
+    )
+
+    const comp = mount(<MyComp num={2} />)
+    expect(comp.text()).toBe('2')
   })
 })
--- src/view.js
+++ src/view.js
@@ -1,55 +1,60 @@
-import { Component } from 'react'
-import { observe, unobserve } from '@nx-js/observer-util'
+import { Component } from 'react';
+import { observe, unobserve, raw, isObservable } from '@nx-js/observer-util';
 
-export default function view (Comp, { devtool: rawDevtool } = {}) {
-  const isStatelessComp = !(Comp.prototype && Comp.prototype.isReactComponent)
-  const BaseComp = isStatelessComp ? Component : Comp
+const COMPONENT = Symbol('owner component');
+const DUMMY_STATE = {};
+
+export default function view(Comp, { devtool: rawDevtool } = {}) {
+  const isStatelessComp = !(Comp.prototype && Comp.prototype.isReactComponent);
+  const BaseComp = isStatelessComp ? Component : Comp;
 
   const devtool = rawDevtool
     ? operation => rawDevtool(Object.assign({ Component: Comp }, operation))
-    : undefined
+    : undefined;
 
   // return a HOC which overwrites render, shouldComponentUpdate and componentWillUnmount
   // it decides when to run the new reactive methods and when to proxy to the original methods
   class ReactiveHOC extends BaseComp {
-    constructor (props, context) {
-      super(props, context)
+    constructor(props, context) {
+      super(props, context);
 
+      this.state = this.state || {};
+      this.state[COMPONENT] = this;
       // create a reactive render for the component
       // run a dummy setState to schedule a new reactive render, avoid forceUpdate
       this.render = observe(this.render, {
-        scheduler: () => this.setState({}),
+        scheduler: () => this.setState(DUMMY_STATE),
         debugger: devtool,
         lazy: true
-      })
+      });
     }
 
-    render () {
-      return isStatelessComp ? Comp(this.props, this.context) : super.render()
+    render() {
+      return isStatelessComp ? Comp(this.props, this.context) : super.render();
     }
 
     // react should trigger updates on prop changes, while easyState handles store changes
-    shouldComponentUpdate (nextProps, nextState) {
-      const { props, state } = this
+    shouldComponentUpdate(nextProps, nextState) {
+      const { props, state } = this;
 
       // respect the case when user prohibits updates
       if (
         super.shouldComponentUpdate &&
         !super.shouldComponentUpdate(nextProps, nextState)
       ) {
-        devtool && devtool({ type: 'render', renderType: 'blocked' })
-        return false
+        devtool && devtool({ type: 'render', renderType: 'blocked' });
+        return false;
       }
 
       // return true if it is a reactive render or state changes
       if (state !== nextState) {
-        devtool && devtool({ type: 'render', renderType: 'reactive' })
-        return true
+        devtool && devtool({ type: 'render', renderType: 'reactive' });
+        return true;
       }
 
       // the component should update if any of its props shallowly changed value
-      const keys = Object.keys(props)
-      const nextKeys = Object.keys(nextProps)
+      const keys = Object.keys(props);
+      const nextKeys = Object.keys(nextProps);
       if (
         nextKeys.length !== keys.length ||
         nextKeys.some(key => props[key] !== nextProps[key])
@@ -60,30 +65,55 @@ export default function view (Comp, { devtool: rawDevtool } = {}) {
             renderType: 'normal',
             props: nextProps,
             oldProps: props
-          })
-        return true
+          });
+        return true;
       }
-      return false
+      return false;
     }
 
-    componentWillUnmount () {
+    // add a custom deriveStoresFromProps lifecyle method
+    static getDerivedStateFromProps(props, state) {
+      if (super.deriveStoresFromProps) {
+        // inject all local stores and let the user mutate them directly
+        const stores = mapStateToStores(state);
+        super.deriveStoresFromProps(props, ...stores);
+      }
+      // respect user defined getDerivedStateFromProps
+      if (super.getDerivedStateFromProps) {
+        return super.getDerivedStateFromProps(props, state);
+      }
+      return null;
+    }
+
+    componentWillUnmount() {
       // call user defined componentWillUnmount
       if (super.componentWillUnmount) {
-        super.componentWillUnmount()
+        super.componentWillUnmount();
       }
       // clean up memory used by Easy State
-      unobserve(this.render)
+      unobserve(this.render);
     }
   }
 
-  ReactiveHOC.displayName = Comp.displayName || Comp.name
+  ReactiveHOC.displayName = Comp.displayName || Comp.name;
   // static props are inherited by class components,
   // but have to be copied for function components
   if (isStatelessComp) {
     for (let key of Object.keys(Comp)) {
-      ReactiveHOC[key] = Comp[key]
+      ReactiveHOC[key] = Comp[key];
     }
   }
 
-  return ReactiveHOC
+  return ReactiveHOC;
+}
+
+function mapStateToStores(state) {
+  // find store properties and map them to their none observable raw value
+  // to do not trigger none static this.setState calls
+  // from the static getDerivedStateFromProps lifecycle method
+  const component = state[COMPONENT];
+  return Object.keys(component)
+    .map(key => component[key])
+    .filter(isObservable)
+    .map(raw);
 }
