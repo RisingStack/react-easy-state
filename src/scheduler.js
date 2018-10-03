e82815850e010b05fe3f0123978de1dde917d537
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-10-03 13:34:45 +0200

    remove DOM specific code parts from scheduler

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,7 +1,7 @@
 const tasks = new Set()
 let isStopped = false
 
-export function add (task) {
+export function add(task) {
   if (isStopped) {
     tasks.add(task)
   } else {
@@ -9,45 +9,56 @@ export function add (task) {
   }
 }
 
-export function remove (task) {
+export function remove(task) {
   tasks.delete(task)
 }
 
 // this replaces the passed function with a function
 // that batches all of its callback arguments
-function batch (fn) {
-  return function batchingCallbacks (...args) {
+function batch(fn) {
+  return function batchingCallbacks(...args) {
     const batchedArgs = args.map(
       arg =>
         typeof arg === 'function'
-          ? function batchedCallback () {
-            try {
-              isStopped = true
-              return arg.apply(this, arguments)
-            } finally {
-              tasks.forEach(runTask)
-              tasks.clear()
-              isStopped = false
+          ? function batchedCallback() {
+              try {
+                isStopped = true
+                return arg.apply(this, arguments)
+              } finally {
+                tasks.forEach(runTask)
+                tasks.clear()
+                isStopped = false
+              }
             }
-          }
           : arg
     )
     return fn.apply(this, batchedArgs)
   }
 }
 
-function runTask (task) {
+function runTask(task) {
   task()
+}
+
+// try to find the global object
+// it is window in the DOM and global in NodeJS and React Native
+let globalObj
+if (typeof window !== 'undefined') {
+  globalObj = window
+} else if (typeof global !== 'undefined') {
+  globalObj = global
 }
 
 // do a sync batching for the most common task sources
 // this should be removed when React's own batching is improved in the future
-window.setTimeout = batch(window.setTimeout)
-window.setInterval = batch(window.setInterval)
-// eslint-disable-next-line
-Promise.prototype.then = batch(Promise.prototype.then);
-// eslint-disable-next-line
-Promise.prototype.catch = batch(Promise.prototype.catch);
+if (globalObj) {
+  globalObj.setTimeout = batch(globalObj.setTimeout)
+  globalObj.setInterval = batch(globalObj.setInterval)
+  // eslint-disable-next-line
+  Promise.prototype.then = batch(Promise.prototype.then)
+  // eslint-disable-next-line
+  Promise.prototype.catch = batch(Promise.prototype.catch)
+}
 
 // DOM event handlers and HTTP event handlers don't have to be batched
 // event handlers are batched by React
