9e3c93071981f107af55647af3afd4cc554d7b04
Miklos Bertalan <miklos.bertalan@risingstack.com>
2019-01-28 17:20:40 +0100

    refactor batching

--- src/scheduler.js
+++ src/scheduler.js
@@ -7,7 +7,7 @@ let batchCount = 0
 // and order them by this num before running them
 // use a sparse array instead of a set -> will work perfectly!!
 // each fn will have a simple id -> it will be ordered and uniqueu
-export function add (task) {
+export function add(task) {
   if (batchCount !== 0) {
     tasks[task.priority] = task
   } else {
@@ -15,21 +15,21 @@ export function add (task) {
   }
 }
 
-export function remove (task) {
+export function remove(task) {
   delete tasks[task.priority]
 }
 
 // must remove the task before executing it
 // otherwise executed task could start a new task list execution
 // and recursively run itself infinite times
-function runTask (task) {
+function runTask(task) {
   remove(task)
   task()
 }
 
 // this runs the passed function and delays all re-renders
 // until the function is finished running
-export function batch (fn, ctx, args) {
+export function batch(fn, ctx, args) {
   try {
     batchCount++
     return fn.apply(ctx, args)
@@ -48,13 +48,13 @@ export function batch (fn, ctx, args) {
 // the cache is necessary to always map the same thing to the same function
 // which makes sure that addEventListener/removeEventListener pairs don't break
 const cache = new Map()
-function batchFn (fn) {
+function batchFn(fn) {
   if (typeof fn !== 'function') {
     return fn
   }
   let batched = cache.get(fn)
   if (!batched) {
-    batched = function (...args) {
+    batched = function(...args) {
       return batch(fn, this, args)
     }
     cache.set(fn, batched)
@@ -63,18 +63,18 @@ function batchFn (fn) {
 }
 
 // batched window.addEventListener(cb) like callbacks
-function batchCallbacks (functionWithCallbacks) {
-  return function batchedCallbacks (...args) {
+function batchCallbacks(functionWithCallbacks) {
+  return function batchedCallbacks(...args) {
     return functionWithCallbacks.apply(this, args.map(batchFn))
   }
 }
 
 // batches obj.onevent = fn like calls
-function batchMethod (obj, method) {
+function batchMethod(obj, method) {
   const descriptor = Object.getOwnPropertyDescriptor(obj, method)
   if (descriptor) {
     const newDescriptor = Object.assign({}, descriptor, {
-      set (value) {
+      set(value) {
         return descriptor.set.call(this, batchFn(value))
       }
     })
@@ -94,6 +94,7 @@ if (typeof window !== 'undefined') {
 // do a sync batching for the most common task sources
 // this should be removed when React's own batching is improved in the future
 if (globalObj) {
+  // batch timer functions
   globalObj.setTimeout = batchCallbacks(globalObj.setTimeout)
   globalObj.setInterval = batchCallbacks(globalObj.setInterval)
   if (globalObj.requestAnimationFrame) {
@@ -106,10 +107,21 @@ if (globalObj) {
       globalObj.requestIdleCallback
     )
   }
+
   // eslint-disable-next-line
-  Promise.prototype.then = batchCallbacks(Promise.prototype.then);
+  Promise.prototype.then = batchCallbacks(Promise.prototype.then)
   // eslint-disable-next-line
-  Promise.prototype.catch = batchCallbacks(Promise.prototype.catch);
+  Promise.prototype.catch = batchCallbacks(Promise.prototype.catch)
+
+  // batch addEventListener calls
+  if (globalObj.EventTarget) {
+    EventTarget.prototype.addEventListener = batchCallbacks(
+      EventTarget.prototype.addEventListener
+    )
+    EventTarget.prototype.removeEventListener = batchCallbacks(
+      EventTarget.prototype.removeEventListener
+    )
+  }
 
   // this batches websocket event handlers
   if (globalObj.WebSocket) {
@@ -118,16 +130,5 @@ if (globalObj) {
       batchMethod(globalObj.WebSocket.prototype, method)
     )
   }
+  // HTTP event handlers are usually wrapped by Promises, which is covered above
 }
-
-// batch addEventListener calls
-if (globalObj.EventTarget) {
-  EventTarget.prototype.addEventListener = batchCallbacks(
-    EventTarget.prototype.addEventListener
-  )
-  EventTarget.prototype.removeEventListener = batchCallbacks(
-    EventTarget.prototype.removeEventListener
-  )
-}
-
-// HTTP event handlers are usually wrapped by Promises, which is covered above
