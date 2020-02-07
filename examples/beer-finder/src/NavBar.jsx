27756d80ce034a02fa72eaafc4d57a59cde840d4
Roland Szoke <fwalkwithm@gmail.com>
2020-02-07 11:24:42 +0100

    fix(standard): ignore examples folder

--- examples/beer-finder/src/NavBar.jsx
+++ examples/beer-finder/src/NavBar.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { view } from 'react-easy-state'
 import SearchBar from 'material-ui-search-bar'
 import LinearProgress from '@material-ui/core/LinearProgress'
-import { fetchBeers as handleOnRequestSearch, isLoading } from './appStore'
+import appStore from './appStore'
 
 // this is re-rendered whenever the relevant parts of the used data stores change
 export default view(() => (
   <div className='searchbar'>
     <SearchBar
-      onRequestSearch={handleOnRequestSearch}
+      onRequestSearch={appStore.fetchBeers}
       placeholder='Add some food ...'
       autoFocus
     />
-    {isLoading && <LinearProgress />}
+    {appStore.isLoading && <LinearProgress />}
   </div>
 ))
