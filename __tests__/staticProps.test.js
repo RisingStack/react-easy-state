a0e11e8eddf5684820f66a9ea49a7f2586219f7a
Miklos Bertalan <miklos.bertalan@risingstack.com>
2020-02-13 10:39:10 +0100

    test(static-props): Improve static props proxying test cases

--- __tests__/staticProps.test.js
+++ __tests__/staticProps.test.js
@@ -2,7 +2,7 @@ import { Component } from 'react'
 import { view } from 'react-easy-state'
 
 describe('static props', () => {
-  test('view() should proxy react specific static properties from the component', () => {
+  test('view() should proxy static properties from wrapped components', () => {
     class Comp extends Component {}
     function FuncComp () {}
 
@@ -10,6 +10,7 @@ describe('static props', () => {
     Comp.contextTypes = FuncComp.contextTypes = {}
     Comp.propTypes = FuncComp.propTypes = {}
     Comp.defaultProps = FuncComp.defaultProps = {}
+    Comp.customProp = FuncComp.customProp = {}
 
     const ViewComp = view(Comp)
     const ViewFuncComp = view(FuncComp)
@@ -18,21 +19,41 @@ describe('static props', () => {
     expect(ViewComp.contextTypes).toBe(Comp.contextTypes)
     expect(ViewComp.propTypes).toBe(Comp.propTypes)
     expect(ViewComp.defaultProps).toBe(Comp.defaultProps)
+    expect(ViewComp.customProp).toBe(Comp.customProp)
 
     expect(ViewFuncComp.displayName).toBe(FuncComp.displayName)
     expect(ViewFuncComp.contextTypes).toBe(FuncComp.contextTypes)
     expect(ViewFuncComp.propTypes).toBe(FuncComp.propTypes)
     expect(ViewFuncComp.defaultProps).toBe(FuncComp.defaultProps)
+    expect(ViewFuncComp.customProp).toBe(FuncComp.customProp)
   })
 
-  test('view() should proxy react specific static getters', () => {
+  test('view() should proxy static methods', () => {
     class Comp extends Component {
-      static get defaultProps () {
+      static getDerivedStateFromError () {}
+      static customMethod () {}
+    }
+
+    const ViewComp = view(Comp)
+    expect(ViewComp.getDerivedStateFromError).toBe(
+      Comp.getDerivedStateFromError
+    )
+    expect(ViewComp.customMethod).toBe(Comp.customMethod)
+  })
+
+  test('view() should proxy static getters', () => {
+    class Comp extends Component {
+      static get defaultProp () {
         return { key: 'value' }
+      }
+
+      static get customProp () {
+        return { key: 'hello' }
       }
     }
 
     const ViewComp = view(Comp)
     expect(ViewComp.defaultProps).toEqual(Comp.defaultProps)
+    expect(ViewComp.customProp).toEqual(Comp.customProp)
   })
 })
