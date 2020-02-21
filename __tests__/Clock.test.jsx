7ef4743a64dfe467c9992ca066d6c0e13c157399
Roland <fwalkwithm@gmail.com>
2020-02-21 16:36:03 +0100

    ci(lint): lint tests before commits
    * refactor(tests): lint test files

---
+++ __tests__/Clock.test.jsx
@@ -0,0 +1,40 @@
+import React, { StrictMode } from 'react';
+import { render, cleanup, act } from '@testing-library/react/pure';
+import sinon from 'sinon';
+import App from '../examples/clock/src/App';
+
+describe('Clock App', () => {
+  const clock = sinon.useFakeTimers();
+  const { container, unmount } = render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+
+  const clearIntervalSpy = sinon.spy(global, 'clearInterval');
+
+  afterAll(() => {
+    cleanup();
+    clock.restore();
+    clearIntervalSpy.restore();
+  });
+
+  test('should update to display the current time every second', () => {
+    expect(container).toHaveTextContent('12:00:00 AM');
+
+    act(() => {
+      clock.tick(2000);
+    });
+    expect(container).toHaveTextContent('12:00:02 AM');
+
+    act(() => {
+      clock.tick(8500);
+    });
+    expect(container).toHaveTextContent('12:00:10 AM');
+  });
+
+  test('should clean up the interval timer when the component is unmounted', () => {
+    unmount();
+    expect(clearIntervalSpy.callCount).toBe(1);
+  });
+});
---
+++ __tests__/Clock.test.native.jsx
@@ -0,0 +1,40 @@
+import React, { StrictMode } from 'react';
+import {
+  render,
+  flushMicrotasksQueue,
+} from 'react-native-testing-library';
+import sinon from 'sinon';
+import App from '../examples/native-clock/App';
+
+describe('Clock App', () => {
+  const clock = sinon.useFakeTimers();
+  const { getByText, unmount } = render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+  // flush the inital didMount effect
+  flushMicrotasksQueue();
+
+  const clearIntervalSpy = sinon.spy(global, 'clearInterval');
+
+  afterAll(() => {
+    clock.restore();
+    clearIntervalSpy.restore();
+  });
+
+  test('should update to display the current time every second', () => {
+    expect(getByText('12:00:00 AM')).toBeDefined();
+
+    clock.tick(2000);
+    expect(getByText('12:00:02 AM')).toBeDefined();
+
+    clock.tick(8500);
+    expect(getByText('12:00:10 AM')).toBeDefined();
+  });
+
+  test('should clean up the interval timer when the component is unmounted', () => {
+    unmount();
+    expect(clearIntervalSpy.callCount).toBe(1);
+  });
+});
