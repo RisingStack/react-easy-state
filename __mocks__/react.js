152ec88d863a5307901444cdae4809bd219fa1ef
Roland Szoke <fwalkwithm@gmail.com>
2020-02-24 15:05:53 +0100

    fix(store): throw error when hooks used below react 16.8

--- __mocks__/react.js
+++ __mocks__/react.js
@@ -1,3 +1,6 @@
 // this is here to avoid duplicate react entries in the examples
 // (one from the example's node_modules and one from the root's node_modules)
-module.exports = require('react');
+
+module.exports = process.env.NOHOOK
+  ? require('no-hook-react')
+  : require('react');
