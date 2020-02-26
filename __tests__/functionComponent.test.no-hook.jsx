877b1ab1d4bb74178b0969d3194b673b683ab3ae
Roland Szoke <fwalkwithm@gmail.com>
2020-02-26 12:41:52 +0100

    fix(store): throw error on functional local store under react 16.8

--- __tests__/functionComponent.test.no-hook.jsx
+++ __tests__/functionComponent.test.no-hook.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
-import { render } from '@testing-library/react/pure';
+import React, { Component } from 'react';
+import { render, cleanup } from '@testing-library/react/pure';
 // eslint-disable-next-line import/no-unresolved
 import { view, store } from 'react-easy-state';
 
 describe('Using an old react version', () => {
+  afterEach(cleanup);
+
   test('Using local state in a function component with a version of react that has no hooks should throw an error', () => {
     const MyComp = view(() => {
       const person = store({ name: 'Bob' });
       return <div>{person.name}</div>;
     });
-
     expect(() => render(<MyComp />)).toThrow(
       'You cannot use state inside a function component with a pre-hooks version of React. Please update your React version to at least v16.8.0 to use this feature.',
     );
+  });
+
+  test('Using global state in a function component with a version of react that has no hooks should not throw an error', () => {
+    const person = store({ name: 'Bob' });
+    const MyComp = view(() => {
+      return <div>{person.name}</div>;
+    });
+    expect(() => render(<MyComp />)).not.toThrow();
+  });
+
+  test('Using global state in a class component with a version of react that has no hooks should not throw an error', () => {
+    const person = store({ name: 'Bob' });
+    const MyComp = view(
+      class extends Component {
+        render() {
+          return <div>{person.name}</div>;
+        }
+      },
+    );
+    expect(() => render(<MyComp />)).not.toThrow();
+  });
+
+  test('Using local state in a class component with a version of react that has no hooks should not throw an error', () => {
+    const MyComp = view(
+      class extends Component {
+        person = store({ name: 'Bob' });
+
+        render() {
+          return <div>{this.person.name}</div>;
+        }
+      },
+    );
+
+    expect(() => render(<MyComp />)).not.toThrow();
   });
 });
