daa49028185a15518d058cc4ab1056b6db52b6b6
Roland Szoke <fwalkwithm@gmail.com>
2020-03-05 16:48:16 +0100

    test(store): merge store test files

--- __tests__/store.no-hook.test.jsx
+++ __tests__/store.no-hook.test.jsx
@@ -6,14 +6,23 @@ import { view, store } from 'react-easy-state';
 describe('Using an old react version', () => {
   afterEach(cleanup);
 
-  test('Using local state in a function component with a version of react that has no hooks should throw an error', () => {
+  test(`Using local state in a function component ${
+    process.env.NOHOOK
+      ? 'with a version of react that has no hooks should'
+      : 'should not'
+  } throw an error`, () => {
     const MyComp = view(() => {
       const person = store({ name: 'Bob' });
       return <div>{person.name}</div>;
     });
-    expect(() => render(<MyComp />)).toThrow(
-      'You cannot use state inside a function component with a pre-hooks version of React. Please update your React version to at least v16.8.0 to use this feature.',
-    );
+
+    if (process.env.NOHOOK) {
+      expect(() => render(<MyComp />)).toThrow(
+        'You cannot use state inside a function component with a pre-hooks version of React. Please update your React version to at least v16.8.0 to use this feature.',
+      );
+    } else {
+      expect(() => render(<MyComp />)).not.toThrow();
+    }
   });
 
   test('Using global state in a function component should not throw an error', () => {
